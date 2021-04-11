70c049b5612ed842cb66a711fc51c9b9d5ea95fc
ChristmasKey <1046280246@qq.com>
2021-04-11 21:32:55 +0800

    购物车列表的item展示

--- src/views/cart/childComps/CartListItem.vue
+++ src/views/cart/childComps/CartListItem.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-    {{product}}
+  <div id="shop-item">
+    <div class="item-selector">
+      <check-button :is-checked="itemInfo.checked" @click.native="checkClick"/>
+    </div>
+    <div class="item-img">
+      <img :src="itemInfo.image" alt="商品图片"/>
+    </div>
+    <div class="item-info">
+      <div class="item-title">{{itemInfo.title}}</div>
+      <div class="item-desc">{{itemInfo.desc}}</div>
+      <div class="item-bottom">
+        <div class="item-price left">￥{{itemInfo.price}}</div>
+        <div class="item-count right">x{{itemInfo.count}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import CheckButton from 'components/content/CheckButton'
+
 export default {
   name: 'CartListItem',
   props: {
-    product: {
-      // type: Object,
-      type: String,
+    itemInfo: {
+      type: Object,
+      // type: String,
       default() {
         return {}
       }
+    }
+  },
+  components: {
+    CheckButton
+  },
+  methods: {
+    checkClick() {
+      this.itemInfo.checked = !this.itemInfo.checked;
     }
   }
 }
